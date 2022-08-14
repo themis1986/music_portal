55d3b4a538f226faf332d034b0c85380cfa780d3
thiakovidis <thiakovidis@agroapps.gr>
2022-08-14 18:51:22 +0300

    Connecting firebase project with the frontend

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDbxMupbMu8GCx7LnELcMzvAzCbn0pQrwg",
+  authDomain: "music-portal-e5e70.firebaseapp.com",
+  projectId: "music-portal-e5e70",
+  storageBucket: "music-portal-e5e70.appspot.com",
+  messagingSenderId: "658785276286",
+  appId: "1:658785276286:web:4ffaeb0baeb2129736e32f",
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+// timestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectAuth, projectFirestore, timestamp };
