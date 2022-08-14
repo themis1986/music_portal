b9fdf9cb9e9768e004d2ac5337548c29a31b9514
thiakovidis <thiakovidis@agroapps.gr>
2022-08-14 19:12:26 +0300

    Adding login UI form

---
+++ src/views/auth/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <h3>Login</h3>
+    <input type="email" placeholder="email" v-model="email" />
+    <input type="password" placeholder="password" v-model="password" />
+    <div v-if="error" class="error">{{ error }}</div>
+    <button>Log in</button>
+  </form>
+</template>
+
+<script>
+import useLogin from "@/composables/useLogin";
+import { ref } from "vue";
+export default {
+  setup() {
+    const { error, login } = useLogin();
+
+    const email = ref("");
+    const password = ref("");
+
+    const handleSubmit = async () => {
+      const response = await login(email.value, password.value);
+      if (!error.value) {
+        console.log(response);
+      }
+    };
+
+    return { email, password, handleSubmit, error };
+  },
+};
+</script>
+
+<style></style>
