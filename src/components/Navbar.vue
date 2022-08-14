36e09f52f6ee8d09f4d35badde5578921c4c6969
thiakovidis <thiakovidis@agroapps.gr>
2022-08-14 20:15:37 +0300

    Adding navigation bar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="navbar">
+    <nav>
+      <img src="@/assets/logo.png" alt="logo" />
+      <h1><router-link :to="{ name: 'Home' }">Music Portal</router-link></h1>
+      <div class="links">
+        <button>Logout</button>
+        <router-link class="btn" :to="{ name: 'Signup' }">Signup</router-link>
+        <router-link class="btn" :to="{ name: 'Login' }">Log in</router-link>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+.navbar {
+  padding: 16px 10px;
+  margin-bottom: 60px;
+  background: #fff;
+}
+
+nav {
+  display: flex;
+  align-items: center;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+
+nav h1 {
+  margin-left: 20px;
+}
+
+nav .links {
+  margin-left: auto;
+}
+
+nav .links a,
+button {
+  margin-left: 16px;
+  font-size: 14px;
+}
+
+img {
+  max-height: 100px;
+  background: #999;
+  border-radius: 50%;
+}
+</style>
