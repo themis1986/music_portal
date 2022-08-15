c9a1e133ff36cfef8df67e1d599ea1fd0c4662e8
thiakovidis <thiakovidis@agroapps.gr>
2022-08-15 19:32:21 +0300

    Querrying playlists by user functionality

--- src/views/playlists/UserPlaylists.vue
+++ src/views/playlists/UserPlaylists.vue
@@ -5,9 +5,21 @@
 </template>
 
 <script>
+import getUser from "@/composables/getUser";
+import getCollection from "@/composables/getCollection";
+
 export default {
   setup() {
-    return {};
+    const { user } = getUser();
+    const { documents: playlists } = getCollection("playlists", [
+      "userId",
+      "==",
+      user.value.uid,
+    ]);
+
+    console.log(playlists);
+
+    return { playlists };
   },
 };
 </script>
