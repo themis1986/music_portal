ed843e24d801d97f1372bd5d97025c4724d0de58
thiakovidis <thiakovidis@agroapps.gr>
2022-08-15 15:50:03 +0300

    Creating playlist details component

---
+++ src/views/playlists/PlaylistDetails.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2>Playlist Details ID: {{ id }}</h2>
+</template>
+
+<script>
+export default {
+  props: ["id"],
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style></style>
