1a84179ed886aea58cc70f60ac6939d152dcbea3
thiakovidis <thiakovidis@agroapps.gr>
2022-11-23 16:08:13 +0200

    Add general instructions and github link in login page

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,13 @@
   <div class="content">
     <router-view />
   </div>
+  <a
+    class="github"
+    href="https://github.com/themis1986/vue3_compositionApi_music_portal"
+    target="_blank"
+    ><span>Github link</span>
+    <img src="./assets/icons8-github-48.png" alt="gihub link" />
+  </a>
 </template>
 
 <script>
@@ -17,5 +24,17 @@ export default {
   margin: 0 auto;
   max-width: 1200px;
   padding: 0 20px;
+}
+
+.github {
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  display: flex;
+  align-items: center;
+}
+.github span {
+  font-size: 10px;
+  margin-bottom: 20px;
 }
 </style>
--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,12 +1,32 @@
 <template>
-  <form @submit.prevent="handleSubmit">
-    <h3>Login</h3>
-    <input type="email" placeholder="email" v-model="email" />
-    <input type="password" placeholder="password" v-model="password" />
-    <div v-if="error" class="error">{{ error }}</div>
-    <button v-if="!isPending">Log in</button>
-    <button v-if="isPending" disabled>Loading</button>
-  </form>
+  <div class="container">
+    <div class="">
+      <form @submit.prevent="handleSubmit">
+        <h3>Login</h3>
+        <input type="email" placeholder="email" v-model="email" />
+        <input type="password" placeholder="password" v-model="password" />
+        <div v-if="error" class="error">{{ error }}</div>
+        <button v-if="!isPending">Log in</button>
+        <button v-if="isPending" disabled>Loading</button>
+      </form>
+    </div>
+    <div class="instructions">
+      <p>
+        You can login to this demo project with the usernam and password
+        credentials as shown below:
+      </p>
+      <ul>
+        <li>Username: themis@gmail.com</li>
+        <li>Password: 111111</li>
+      </ul>
+      <br />
+      <p>
+        Alternatively you can create your own custom account in the
+        <router-link class="link" :to="{ name: 'Signup' }">Signup</router-link>
+        form using a dummy email and password.
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -34,4 +54,19 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.container {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  column-gap: 10px;
+}
+.instructions {
+  align-self: center;
+}
+.link {
+  border: 1px solid rgb(245, 235, 101);
+  padding: 5px 10px;
+  background: #eee;
+  border-radius: 10px;
+}
+</style>
